0d584a24f26a5adf70b6f4e23ee4ccc10b825a08
Mateusz Mikulski <mikulskee@gmail.com>
2019-11-19 21:47:56 +0100

    changed route animation for StatsTemplate

--- src/animations/index.js
+++ src/animations/index.js
@@ -2,11 +2,20 @@ import { TimelineMax as Timeline, Power1 } from "gsap";
 
 const getHomeEnterTimeline = node => {
   const timeline = new Timeline({ paused: true });
-  timeline.from(node, 0.35, {
-    autoAlpha: 0,
-    scale: 1.1,
-    ease: Power1.easeOut
-  });
+  const cards = node.querySelectorAll(".stats-tournament-card");
+  timeline
+    .from(node, 0.35, {
+      autoAlpha: 0,
+      scale: 1.1,
+      ease: Power1.easeOut
+    })
+    .staggerFrom(
+      cards,
+      0.3,
+      { autoAlpha: 0, x: -25, ease: Power1.easeOut },
+      0.1
+    );
+
   return timeline;
 };
 
