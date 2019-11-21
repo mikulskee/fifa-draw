cb7e0f7c9c352722f465d56e0eca8395b4baa106
Mateusz Mikulski <mikulskee@gmail.com>
2019-11-21 23:29:57 +0100

    Updates

---
+++ precache-manifest.4aa73bb012ef02c9773eb0c8303e3a25.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b7b6fb6c5e2f401d511b8b14800147",
+    "url": "/fifa-draw/index.html"
+  },
+  {
+    "revision": "7a5a4cc2459261887184",
+    "url": "/fifa-draw/static/css/main.6e755607.chunk.css"
+  },
+  {
+    "revision": "8b6b6abf9b358b2343ef",
+    "url": "/fifa-draw/static/js/2.15c3cca5.chunk.js"
+  },
+  {
+    "revision": "7a5a4cc2459261887184",
+    "url": "/fifa-draw/static/js/main.aec8d4d2.chunk.js"
+  },
+  {
+    "revision": "9453c1e4f257ceefb5df",
+    "url": "/fifa-draw/static/js/runtime-main.86f760c9.js"
+  },
+  {
+    "revision": "3e16f35d19ad7bb21069cc8583b87a6c",
+    "url": "/fifa-draw/static/media/arsenal.3e16f35d.png"
+  },
+  {
+    "revision": "8bb3aa449f42076dfbfc433572d35e17",
+    "url": "/fifa-draw/static/media/barca.8bb3aa44.png"
+  },
+  {
+    "revision": "e187c8cb99ab1f25b3e417d14aa527aa",
+    "url": "/fifa-draw/static/media/bayer.e187c8cb.png"
+  },
+  {
+    "revision": "93c8489f914c2b23290d488761d1ba71",
+    "url": "/fifa-draw/static/media/bayern.93c8489f.png"
+  },
+  {
+    "revision": "dc37ec4db93c3ae742b7f48b6ad84ae3",
+    "url": "/fifa-draw/static/media/bvb.dc37ec4d.png"
+  },
+  {
+    "revision": "85dd030d0aafd2c099e5e525fd9ca1ee",
+    "url": "/fifa-draw/static/media/chelsea.85dd030d.png"
+  },
+  {
+    "revision": "c11faa01d528b95afbc7ff0e42a8bb5e",
+    "url": "/fifa-draw/static/media/city.c11faa01.png"
+  },
+  {
+    "revision": "ddb0cc7e1e47b7caa7ad1356aeb49995",
+    "url": "/fifa-draw/static/media/inter.ddb0cc7e.png"
+  },
+  {
+    "revision": "703cd1cfba7986cda595028671c60e70",
+    "url": "/fifa-draw/static/media/lipsk.703cd1cf.png"
+  },
+  {
+    "revision": "4088e2bc983159f4973505e7de43c6b3",
+    "url": "/fifa-draw/static/media/liverpool.4088e2bc.png"
+  },
+  {
+    "revision": "9c30fcde4b0cfc54e6a58cb91adcf35d",
+    "url": "/fifa-draw/static/media/logo-white-png.9c30fcde.png"
+  },
+  {
+    "revision": "879a2d774d4311dbafaaf013a8e3b642",
+    "url": "/fifa-draw/static/media/main-bg.879a2d77.jpg"
+  },
+  {
+    "revision": "75f441e721377a14290dc03f5c16dae8",
+    "url": "/fifa-draw/static/media/milan.75f441e7.png"
+  },
+  {
+    "revision": "455f11bc05c74b846554937c5a4305be",
+    "url": "/fifa-draw/static/media/monaco.455f11bc.png"
+  },
+  {
+    "revision": "9591566f22439eba70ae530fa366033b",
+    "url": "/fifa-draw/static/media/napoli.9591566f.png"
+  },
+  {
+    "revision": "d62cccb2347176d6b4f5e22acbe14bdc",
+    "url": "/fifa-draw/static/media/newcup-bg.d62cccb2.png"
+  },
+  {
+    "revision": "93ce4b22ad2378418c49b43282f7ed2e",
+    "url": "/fifa-draw/static/media/psg.93ce4b22.png"
+  },
+  {
+    "revision": "78388485d4874fa58ddf7f35173d367d",
+    "url": "/fifa-draw/static/media/real.78388485.png"
+  },
+  {
+    "revision": "a496a2d7697c1fcc0e0e350c4dac764e",
+    "url": "/fifa-draw/static/media/roma.a496a2d7.png"
+  },
+  {
+    "revision": "b5f4c7aa9eb6b52a3d147cb19ee765a0",
+    "url": "/fifa-draw/static/media/stats-bg.b5f4c7aa.png"
+  },
+  {
+    "revision": "62e768a07539ca483b93cbeca82dbfec",
+    "url": "/fifa-draw/static/media/tottenham.62e768a0.png"
+  },
+  {
+    "revision": "6d4eb6758d0cf618e876c0ffced78b51",
+    "url": "/fifa-draw/static/media/tournament-bg.6d4eb675.png"
+  },
+  {
+    "revision": "7958e9194662d701b91ec02deaec3396",
+    "url": "/fifa-draw/static/media/united.7958e919.png"
+  }
+]);
